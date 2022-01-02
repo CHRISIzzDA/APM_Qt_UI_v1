55ef79bcd27682750ba66634d8d37609d5226b71
Erwin T <62778893+mandMister@users.noreply.github.com>
2022-01-02 17:22:44 +0100

    implemented algorithm for async. serial communication
    added basic structure only

--- serialcomm.cpp
+++ serialcomm.cpp
@@ -2,7 +2,6 @@
 
 SerialComm::SerialComm(QObject *parent) : QObject(parent)
 {
-    connect(_timer, &QTimer::timeout, this, &SerialComm::Test);
 }
 
 SerialComm::~SerialComm()
@@ -20,21 +19,66 @@ void SerialComm::StartTest()
 
 void SerialComm::StopTest()
 {
-    _timer->stop();
     if (_port->isOpen()) _port->close();
 }
 
 void SerialComm::Test()
 {
-    while(running)
+    char* response = NULL;
+    response = (char*) malloc(4 * sizeof(char));
+
+    _measuringTimer->start();
+    while (true)
     {
+        _tickTimer->start();
+        _depthTimer->start();
 
+        QByteArray query = QString::number(_pumpPower.at(_currState)).toLocal8Bit();
+        _port->write(query);
+
+        _port->read(response, 4);
+
+        // TODO: convert to something useful like meters or hamburgers per bald eagles
+        int depth = TO_16_BIT(response[0], response[1]);
+        int flow  = TO_16_BIT(response[2], response[3]);
+
+        // File stuff and data ready
+
+        if (depth <= _depthlimit)
+        {
+            _errcode = ERR_MIN_DEPTH;
+            break;
+        }
+
+        if (_depthfailcount >= 15)
+        {
+            _errcode = ERR_DEPTH_FAIL;
+            break;
+        }
+
+        if (_depthTimer->elapsed() >= (_samplerate * 60000))
+        {
+            if (true) //depth then - depth now >= 5 or somthing
+            {
+                ++_currState;
+
+                if (_currState >= _pumpPower.length())
+                {
+                    break;
+                }
+
+                _depthTimer->start();
+            }
+
+            else if (_currState == 0)
+            {
+                ++_depthfailcount;
+            }
+        }
+
+        while (_tickTimer->elapsed() < 50);
     }
-    // while senden (abhängig vom state) bis es passt
 
-    // lesen
-
-    // signal DataReady1
-
-    // speichern in file
+    free(response);
+    emit TestDone();
 }
--- serialcomm.h
+++ serialcomm.h
@@ -4,7 +4,9 @@
 #include <QObject>
 #include <QSerialPort>
 #include <QTimer>
-#include <thread>
+#include <QElapsedTimer>
+
+#define TO_16_BIT(vH, vL) (((vH) << 8) + (vL))
 
 typedef struct
 {
@@ -22,11 +24,14 @@ public:
     explicit SerialComm(QObject *parent = nullptr);
     ~SerialComm();
 
-    QSerialPort* port() { return _port; }
-    QVector<TestData_t>* data() { return &_dataVec; }
+    QSerialPort& port() { return *_port; }
+    QVector<TestData_t>& data() { return _dataVec; }
+
+    enum ErrorCodes { ERR_NONE, ERR_MIN_DEPTH, ERR_DEPTH_FAIL };
 
 signals:
     void DataReady();
+    void TestDone();
 
 public slots:
     void StartTest();
@@ -38,8 +43,19 @@ private slots:
 
 private:
     QSerialPort* _port = new QSerialPort();
-    QTimer* _timer = new QTimer();
     QVector<TestData_t> _dataVec;
+
+    QElapsedTimer* _tickTimer = new QElapsedTimer();
+    QElapsedTimer* _measuringTimer = new QElapsedTimer();
+    QElapsedTimer* _depthTimer = new QElapsedTimer();
+
+    QList<int> _pumpPower = { 0, 100, 500, 1000, 1024 };
+    int _timeout = 15; // min
+    int _samplerate = 15; //min
+    int _currState = 0;
+    int _depthlimit = 10;
+    int _errcode = ERR_NONE;
+    int _depthfailcount = 0;
 };
 
 #endif // SERIALCOMM_H
